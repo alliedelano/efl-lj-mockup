bca60914e446d5583625930c00388decf3cb219a
Allie Delano <alliedelano@Allies-MacBook-Pro.local>
2022-02-08 16:30:19 -0800

    fixes links in pbl unit list

--- src/components/UnitList/UnitList.jsx
+++ src/components/UnitList/UnitList.jsx
@@ -4,10 +4,10 @@ export default function UnitList(){
     return(
         <>
            <h3>Units</h3>
-           <a href="/pblcourse/pblunit1"><h5>1. What is Project-based Learning?</h5></a>
-           <a href="/pblcourse/pblunit2"><h5>2. Project Design and Assessment</h5></a>
-           <a href="/pblcourse/pblunit3"><h5>3. The Driving Question</h5></a>
-           <a href="/pblcourse/pblunit4"><h5>4. Community, Employers, Authenticity</h5></a>
+           <a href="/courses/pblcourse/pblunit1"><h5>1. What is Project-based Learning?</h5></a>
+           <a href="/courses/pblcourse/pblunit2"><h5>2. Project Design and Assessment</h5></a>
+           <a href="/courses/pblcourse/pblunit3"><h5>3. The Driving Question</h5></a>
+           <a href="/courses/pblcourse/pblunit4"><h5>4. Community, Employers, Authenticity</h5></a>
            <h5>5. Entry Event (not live)</h5>
            <h5>6. PBL Strategies, Protocols, Rubrics (not live)</h5>
            <h5>7. Beautiful Work (not live)</h5>
